3a4654b02f0d7bf0af61ece6c9f934a65e093689
Katherine Whitmoyer <katewhitmoyer@gmail.com>
2023-04-10 15:59:33 -0400

    Added header to product info and cart

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,6 +1,9 @@
+import Header from "../components/Header";
+
 const Cart = () => {
     return (
         <div>
+            <Header collapsed={true}/>
             <h1>Cart</h1>
             <p> Placeholder page for Cart. </p>
         </div>
--- pages/productinfo.jsx
+++ pages/productinfo.jsx
@@ -1,6 +1,9 @@
+import Header from "../components/Header";
+
 const ProductInfo = () => {
     return (
         <div>
+            <Header collapsed={true} />
             <h1>Product Info</h1>
             <p> Placeholder for product info page. </p>
         </div>
