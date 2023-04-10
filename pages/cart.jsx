21ecda0f98c76142dec30ed842c11ef42af28969
Katherine Whitmoyer <katewhitmoyer@gmail.com>
2023-04-10 15:37:51 -0400

    Added placeholders for cart and product info pages

---
+++ pages/cart.jsx
@@ -0,0 +1,10 @@
+const Cart = () => {
+    return (
+        <div>
+            <h1>Cart</h1>
+            <p> Placeholder page for Cart. </p>
+        </div>
+    )
+}
+
+export default Cart;
---
+++ pages/productinfo.jsx
@@ -0,0 +1,10 @@
+const ProductInfo = () => {
+    return (
+        <div>
+            <h1>Product Info</h1>
+            <p> Placeholder for product info page. </p>
+        </div>
+    )
+}
+
+export default ProductInfo;
