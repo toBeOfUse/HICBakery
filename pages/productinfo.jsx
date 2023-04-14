dd8c6442da6d32837d7a5ed269e679be5e2baa23
Katherine Whitmoyer <katewhitmoyer@gmail.com>
2023-04-14 15:25:23 -0400

    Added basics of product info

--- pages/productinfo.jsx
+++ pages/productinfo.jsx
@@ -23,55 +23,37 @@ const ProductInfo = () => {
     <Header collapsed={true} />
     </div>
   
+  {/* Maybe make the results button always bakery blue */}
   <div className="w3-container w3-padding-32 w3-margin-right w3-border-left">
     <div>
-    <a className = "w3-btn w3-transparent w3-border-left" href="https://localhost:3000/search"> <h4>Return to Search Results </h4> </a>
+    <a className = "w3-btn bakery-blue w3-border-left" href="https://localhost:3000/search"> <h4> ← Return to Search Results </h4> </a>
     </div>
   </div>
 
-
-
   {/* Add a background color and large text to the whole page */}
   <div className="w3-white w3-large">
-    {/* About Container */}
-    <div className="w3-container" id="about">
-      <div className="w3-content" style={{ maxWidth: 700 }}>
-        <h5 className="w3-left w3-padding-64">
-          <span className="w3-tag w3-wide">ABOUT THE CAFE</span>
-        </h5>
-        <p>
-          The Cafe was founded in blabla by Mr. Smith in lorem ipsum dolor sit
-          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
-        <p>
-          In addition to our full espresso and brew bar menu, we serve fresh
-          made-to-order breakfast and lunch sandwiches, as well as a selection
-          of sides and salads and other good stuff.
-        </p>
-        <div className="w3-panel w3-leftbar w3-light-grey">
-          <p>
-            <i>
-              "Use products from nature for what it's worth - but never too
-              early, nor too late." Fresh is the new sweet.
-            </i>
-          </p>
-          <p>Chef, Coffeeist and Owner: Liam Brown</p>
-        </div>
-        <img
-          src="https://www.w3schools.com/w3images/coffeeshop.jpg"
-          style={{ width: "100%", maxWidth: 1000 }}
-          className="w3-margin-top"
+   
+  {/* Contains product photo and info */}
+  <div className = "w3-row w3-content">
+    {/* Product Photo */}
+    <div className = "w3-quarter w3-container">
+      <img
+          src="../public/cupcakes.png"
+          style={{ width: "100%", maxWidth: 1000, marginTop: 1 }}
         />
-        <p>
-          <strong>Opening hours:</strong> everyday from 6am to 5pm.
-        </p>
-        <p>
-          <strong>Address:</strong> 15 Adr street, 5015, NY
-        </p>
-      </div>
     </div>
+    {/* Product Infomation*/}
+    <div className = "w3-threequarter w3-container">
+      <ul className = "w3-ul">
+        <li>
+          <span className="w3-cell w3-wide bakery-blue">   Product Info   </span>
+        </li>
+        <li>
+          <h1>test</h1>
+        </li>
+      </ul>
+    </div>
+  </div>
   
   {/* Menu Container */}
     <div className="w3-container" id="menu">
