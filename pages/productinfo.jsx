5f8ebc5bbb0d5dc6104aa229c0d13e883e52eb9e
Katherine Whitmoyer <katewhitmoyer@gmail.com>
2023-04-14 20:32:04 -0400

    Added new menu feature

--- pages/productinfo.jsx
+++ pages/productinfo.jsx
@@ -7,11 +7,7 @@ const ProductInfo = () => {
   <title>Cupcake Corner</title>
   <meta charSet="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Inconsolata"
-  />
+
   <style
     dangerouslySetInnerHTML={{
       __html:
@@ -23,24 +19,30 @@ const ProductInfo = () => {
     <Header collapsed={true} />
     </div>
   
-  {/* Maybe make the results button always bakery blue */}
+  {/* return to results button  */}
   <div className="w3-container w3-padding-32 w3-margin-right w3-border-left">
     <div>
-    <a className = "w3-btn bakery-blue w3-border-left" href="https://localhost:3000/search"> <h4> ← Return to Search Results </h4> </a>
+    <a className = "w3-btn w3-transparent w3-border-left w3-hover-aqua" href="https://localhost:3000/search"> <h4> ← Return to Search Results </h4> </a>
     </div>
   </div>
 
   {/* Add a background color and large text to the whole page */}
   <div className="w3-white w3-large">
    
+
   {/* Contains product photo and info */}
-  <div className = "w3-row w3-content">
+  <div className = "w3-row w3-content ">
     {/* Product Photo */}
     <div className = "w3-quarter w3-container">
-      <img
-          src="../public/cupcakes.png"
-          style={{ width: "100%", maxWidth: 1000, marginTop: 1 }}
+    <img
+          src="https://www.shutterstock.com/image-photo/tasty-cupcakes-on-stand-260nw-778287970.jpg"
+          style={{ width: "100%", maxWidth: 1000 }}
         />
+          <div className = "w3-padding-64"></div>
+          <div className = "w3-padding-64"></div>
+          <div className = "w3-padding-64"></div>
+          <div className = "w3-padding-16"></div>
+          <div> test </div>
     </div>
     {/* Product Infomation*/}
     <div className = "w3-threequarter w3-container">
@@ -52,17 +54,54 @@ const ProductInfo = () => {
         <li>
           <div className="w3-display-container">
           <span className="w3-cell w3-padding-16 w3-xlarge"> A classic vanilla cupcake made with real vanilla and topped with fresh buttercream frosting </span>
-          Add to cart
+
+          {/*Add to Cart component */}
+          <div class="w3-dropdown-hover">
+            <button class="w3-button"> + Add to Cart </button>
+              <div class="w3-dropdown-content w3-bar-block w3-border">
+                <a href="#" class="w3-bar-item w3-button">1</a>
+                <a href="#" class="w3-bar-item w3-button">2</a>
+                <a href="#" class="w3-bar-item w3-button">3</a>
+              </div>
+            </div>
           </div>
         </li>
+
         {/* Container containing ingredients and allergens button*/}
         <li>
           <div className = "w3-row w3-container w3-stretch">
             <div className = "w3-quarter w3-large">
-              Ingredients
+            <button class="w3-btn w3-transparent w3-hover-aqua">Ingredients</button>
             </div>
             <div className = "w3-threequarter w3-large">
-              Allergens
+            <button class="w3-btn w3-transparent w3-hover-aqua">Allergens</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className = "w3-padding-64"></div>
+          <div className = "w3-padding-64"></div>
+          <div>
+          <div className = "w3-container w3-card-2" id="menu">
+            <div className="w3-content">
+              <h5 className = "w3-center"><span className = "w3-tag bakery-blue w3-wide"> Our Favorites</span></h5>
+
+              <div id="Eat" className = "w3-container w3-card-2 menu w3-padding-48"></div>
+                <h5>Bread basket</h5>
+                <p className="w3-text-grey w3-medium"> Product Description</p>
+
+                <h5>Bread basket</h5>
+                <p className="w3-text-grey w3-medium"> Product Description</p>
+
+                <h5>Bread basket</h5>
+                <p className="w3-text-grey w3-medium"> Product Description</p>
+
+                <h5>Bread basket</h5>
+                <p className="w3-text-grey w3-medium"> Product Description</p>
+
+                <h5>Bread basket</h5>
+                <p className="w3-text-grey w3-medium"> Product Description</p>
+              </div>
             </div>
           </div>
         </li>
@@ -70,74 +109,13 @@ const ProductInfo = () => {
     </div>
   </div>
   
-  {/* Menu Container */}
-    <div className="w3-container" id="menu">
-      <div className="w3-content" style={{ maxWidth: 700 }}>
-        <h5 className="w3-center w3-padding-48">
-          <span className="w3-tag w3-wide">THE MENU</span>
-        </h5>
-        <div className="w3-row w3-left w3-card w3-padding">
-          <a
-            href="javascript:void(0)"
-            onclick="openMenu(event, 'Eat');"
-            id="myLink"
-          >
-            <div className="w3-col s6 tablink">Eat</div>
-          </a>
-          <a href="javascript:void(0)" onclick="openMenu(event, 'Drinks');">
-            <div className="w3-col s6 tablink">Drink</div>
-          </a>
-        </div>
-        <div id="Eat" className="w3-container menu w3-padding-48 w3-card">
-          <h5>Bread Basket</h5>
-          <p className="w3-text-grey">
-            Assortment of fresh baked fruit breads and muffins 5.50
-          </p>
-          <br />
-          <h5>Honey Almond Granola with Fruits</h5>
-          <p className="w3-text-grey">
-            Natural cereal of honey toasted oats, raisins, almonds and dates
-            7.00
-          </p>
-          <br />
-          <h5>Belgian Waffle</h5>
-          <p className="w3-text-grey">
-            Vanilla flavored batter with malted flour 7.50
-          </p>
-          <br />
-          <h5>Scrambled eggs</h5>
-          <p className="w3-text-grey">
-            Scrambled eggs, roasted red pepper and garlic, with green onions
-            7.50
-          </p>
-          <br />
-          <h5>Blueberry Pancakes</h5>
-          <p className="w3-text-grey">
-            With syrup, butter and lots of berries 8.50
-          </p>
-        </div>
-        <div id="Drinks" className="w3-container menu w3-padding-48 w3-card">
-          <h5>Coffee</h5>
-          <p className="w3-text-grey">Regular coffee 2.50</p>
-          <br />
-          <h5>Chocolato</h5>
-          <p className="w3-text-grey">Chocolate espresso with milk 4.50</p>
-          <br />
-          <h5>Corretto</h5>
-          <p className="w3-text-grey">Whiskey and coffee 5.00</p>
-          <br />
-          <h5>Iced tea</h5>
-          <p className="w3-text-grey">Hot tea, except not hot 3.00</p>
-          <br />
-          <h5>Soda</h5>
-          <p className="w3-text-grey">Coke, Sprite, Fanta, etc. 2.50</p>
-        </div>
-        <img
-          src="https://www.w3schools.com/w3images/coffeehouse2.jpg"
-          style={{ width: "100%", maxWidth: 1000, marginTop: 32 }}
-        />
-      </div>
-    </div>
+  <div className = "w3-container w3-padding-64">
+  </div>
+
+
+
+  
+
     {/* Contact/Area Container */}
     <div className="w3-container" id="where" style={{ paddingBottom: 32 }}>
       <div className="w3-content" style={{ maxWidth: 700 }}>
