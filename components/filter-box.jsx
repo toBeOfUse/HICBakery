ab7fc4dc2aa8e294666ebaab82a8708048311bfe
Dominic Ferrando <dominicf2001@gmail.com>
2023-04-16 19:04:19 -0400

    Complete intial design and functionality of filter-list

--- components/filter-box.jsx
+++ components/filter-box.jsx
@@ -1,34 +1,18 @@
 import styles from "../styles/search.module.css"
+import CollapsibleList from "./collapsible-list";
 
 export default function FilterBox() {
+    const typeItems = ['Cupcakes', 'Cookies', 'Cakes'];
+    const flavorItems = ['Chocolate', 'Vanilla', 'Caramel', 'Coconut'];
+    const occasionItems = ['Birthday', 'Celebration', 'Casual'];
+
     return (
         <div id={styles.Container}>
             <h1 id={styles.ResultsDisplay}>Results for: Vanilla</h1>
             <ul id={styles.Categories}>
-                <li className={styles.category}>
-                    <h1 className={styles.categoryHeader}>Type</h1>
-                    <ul className={styles.options}>
-                        <li className={styles.option}>Cupcakes</li>
-                        <li className={styles.option}>Cookies</li>
-                        <li className={styles.option}>Cakes</li>
-                    </ul>
-                </li>
-                <li className={styles.category}>
-                    <h1 className={styles.categoryHeader}>Flavor</h1>
-                    <ul className={styles.options}>
-                        <li className={styles.option}>Chocolate</li>
-                        <li className={styles.option}>Vanilla</li>
-                        <li className={styles.option}>Caramel</li>
-                    </ul>
-                </li>
-                <li className={styles.category}>
-                    <h1 className={styles.categoryHeader}>Occasion</h1>
-                    <ul className={styles.options}>
-                        <li className={styles.option}>Birthday</li>
-                        <li className={styles.option}>Celebration</li>
-                        <li className={styles.option}>Casual</li>
-                    </ul>
-                </li>
+                <CollapsibleList header="Type" items={typeItems} />
+                <CollapsibleList header="Flavor" items={flavorItems} />
+                <CollapsibleList header="Occasion" items={occasionItems} />
             </ul>
         </div>
     );
