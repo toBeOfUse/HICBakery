382e6ed4441b1d7f6adbff9b66f1c1406f7cd263
Mitch J <mjacovet@kent.edu>
2023-04-19 15:29:46 -0400

    fix comments

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,15 +2,15 @@ import db from "../../components/db.js";
 
 export default function handler(req, res) {
   // URL:
-  // https://localhost:3000/api/search?keyword=cake
+  // http://localhost:3000/api/search?keyword=cake
   // RESULT:
   // req.query.keyword == "cake"
-  // so if you did https://localhost:3000/api/search?keyword=apple&category_filter=pastry
+  // so if you did http://localhost:3000/api/search?keyword=apple&category_filter=pastry
   // you would get req.query.keyword == "apple" && req.query.category_filter == "pastry"
   const searchForName = req.query.keyword;
   if (searchForName) {
     const results = db
-      .prepare("select * from products where name like ?;")
+      .prepare("select * from products where name like ? and ();")
       .all("%" + searchForName + "%");
     res.json(results);
   } else {
