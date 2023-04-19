5da3623f116745c83abc9cf06bea846e99e30819
Mitch J <mjacovet@kent.edu>
2023-04-19 15:19:05 -0400

    add comments :D

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,6 +1,12 @@
 import db from "../../components/db.js";
 
 export default function handler(req, res) {
+  // URL:
+  // https://localhost:3000/api/search?keyword=cake
+  // RESULT:
+  // req.query.keyword == "cake"
+  // so if you did https://localhost:3000/api/search?keyword=apple&category_filter=pastry
+  // you would get req.query.keyword == "apple" && req.query.category_filter == "pastry"
   const searchForName = req.query.keyword;
   if (searchForName) {
     const results = db
--- pages/search_test.jsx
+++ pages/search_test.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 
 export default function SearchTestPage() {
     const [searchInput, setSearchInput] = useState("");
-    const [searchResults, setSearchResults] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
 
     const doSearch = () => {
+        // compose a URL from state variables:
         fetch("/api/search?keyword=" + searchInput).then(
+            // get the response and store it:
             res => res.json().then(
-                rows => setSearchResults(JSON.stringify(rows, null, 4))
+                rows => setSearchResults(rows)
             )
         );
     };
@@ -16,6 +18,7 @@ export default function SearchTestPage() {
         <input value={searchInput} onChange={e => setSearchInput(e.target.value)} />
         <button onClick={doSearch}>Search</button>
         <p>Search results:</p>
-        <pre>{searchResults}</pre>
+        {/* Turn the results into search result components. (This just turns them into strings) */}
+        <pre>{searchResults.map(r => JSON.stringify(r, null, 4)).join("\n")}</pre>
     </>
 }
