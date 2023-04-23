ffbe5c1830bcfb14805cc33a015e1ca785898c65
Mitch J <mjacovet@kent.edu>
2023-04-23 17:40:50 -0400

    restyle cart

--- components/cart-item.jsx
+++ components/cart-item.jsx
@@ -1,33 +1,29 @@
+import Link from "next/link";
+import { formatPrice } from "../utilities/format";
+import styles from "../styles/cart-item.module.css";
+
 export default function CartItem({ product, quantity_in_cart }) {
-    return <div className="w3-row w3-content">
+    return <div className={styles.container}>
         {/*Photo of product*/}
-        <div className="w3-third w3-containers">
-            <img
-                src={`/product_photos/${product.photo_file_name}`}
-                style={{ width: "100%", maxWidth: 1000 }}
-            />
-        </div>
+        <img
+            src={`/product_photos/${product.photo_file_name}`}
+            className={styles.image}
+        />
 
         {/*Product Description*/}
-        <div className="w3-twothird w3-container">
-            {/* List used to create proper-looking formating */}
-            <ul className="w3-ul">
-                <li>
-                    <span className="w3-cell w3-wide bakery-blue w3-medium w3-threequarters">{product.name}</span>
-                </li>
-                <li>
-                    <span className="w3-cell w3-padding-8 w3-medium">{product.description}</span>
-                    <div className="w3-medium"> Delivery Status: In stock</div>
-                    <div className="w3-row w3-left">
-                        <div className="w3-button w3-half w3-medium w3-padding-top w3-border-bottom">
-                            {quantity_in_cart}
-                        </div>
-                        <div className="w3-left 23-medium w3-padding-8">
-                            Price: ${(product.price / 100).toFixed(2)}
-                        </div>
-                    </div>
-                </li>
-            </ul>
+        <div className={styles.productText}>
+            <h3 className={styles.productName}>
+                <Link href={`/productInfo?product=${product.id}`}>
+                    {product.name}
+                </Link>
+            </h3>
+
+            <p className={styles.productDescription}>{product.description}</p>
+            {/* <div> Delivery Status: In stock</div> */}
+            <div className={styles.smallProductText}>
+                <span>Item price: ${formatPrice(product.price)}</span>
+                <span> Quantity in cart: {quantity_in_cart} </span>
+            </div>
         </div>
-    </div>
+    </div >
 }
--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,39 +1,42 @@
+import Link from "next/link";
 import Header from "../components/Header";
 import CartItem from "../components/cart-item";
+import { formatPrice } from "../utilities/format";
+import styles from "../styles/cart.module.css";
 
 export async function getServerSideProps() {
   const db = (await import("../components/db")).default;
   const cartRows = db.prepare("select * from carts").all();
   const cartItems = [];
+  let totalPrice = 0;
   for (const row of cartRows) {
     const product = db.prepare("select * from products where id=?").get(row.product_id);
     cartItems.push({
       quantity_in_cart: row.quantity,
       product
     });
+    totalPrice += product.price;
   }
   return {
-    props: { cartItems }
+    props: { cartItems, totalPrice }
   };
 }
 
 
-const Cart = ({ cartItems }) => {
+const Cart = ({ cartItems, totalPrice }) => {
   return (
     <>
       <Header collapsed={true} />
 
-      {/* Add a background color and large text to the whole page */}
-      <div className="w3-white w3-large">
+      <div className={styles.container}>
 
         {/*Row for cart compoent and price compoent */}
-        <div className="w3-container">
-          <div className="w3-row">
+        <div>
+          <p className={styles.header}> Your Cart</p>
+          <div className={styles.primaryContentContainer}>
 
             {/*Cart containing row - left half of the page*/}
-            <div className="w3-half w3-container">
-              <div className="w3-padding-16"></div>
-              <span className="w3-cell w3-wide bakery-blue w3-xxlarge w3-border-left"> Your Cart</span>
+            <div>
 
               {cartItems.map((item, i) => <CartItem {...item} key={i} />)}
 
@@ -42,46 +45,22 @@ const Cart = ({ cartItems }) => {
 
             {/*Begin right half of page */}
 
-            <div className="w3-cell w3-half w3-padding-64">
-              <div className="w3-padding-32"></div>
-              <div className="w3-card-2 w3-center w3-margin-right w3-margin-left w3-padding-64 w3-round-medium">
-                <div className="w3-padding-16 w3-large-text">
-                  <span> Total Price: 12.45</span>
-                </div>
-                <div>
-                  <button className="w3-btn bakery-blue w3-hover-aqua w3-padding-16 w3-round-medium">Proceed to Checkout</button>
-                </div>
+            <div className={"w3-card-2 w3-center w3-round-medium " + styles.priceContainer}>
+              <p> Total Price: ${formatPrice(totalPrice)}</p>
+              <div>
+                <Link href="/checkout" className="bakery-blue" style={{ padding: 12 }}>
+                  Proceed to Checkout
+                </Link>
               </div>
             </div>
-
-
-
           </div>
         </div>
 
-
-
-
-        <div className="w3-padding-64"></div>
         {/* End page content */}
       </div>
-
-      {/* Footer. */}
-      <footer className="w3-center bakery-blue w3-padding-48 w3-large">
-
-      </footer>
     </>
-
-
-
-
-
   )
 
 }
-
-
-
-
 
 export default Cart;
