5a1ce74ad6fa8c1227a6dc5beda2677e50b50196
Mitch J <mjacovet@kent.edu>
2023-04-23 21:25:32 -0400

    make checkout page dynamic

--- pages/checkout.jsx
+++ pages/checkout.jsx
@@ -1,52 +1,56 @@
-import Header from "../components/Header"
+import { useMemo } from "react";
+import { useCart } from "../components/CartProvider";
+import Header from "../components/Header";
+import styles from "../styles/checkout.module.css";
+import { formatPrice } from "../utilities/format.js";
 
 const Checkout = () => {
+  const [cart, updateCart] = useCart();
+  const totalPrice = useMemo(() =>
+    cart.reduce(
+      (acc, val) => acc + val.product.price * val.quantity_in_cart, 0
+    ), [cart]);
+
   return (
     <>
       <Header collapsed={true} />
       {/* About Container */}
       <div className="w3-container" id="about">
         <div className="w3-content" style={{ maxWidth: 700 }}>
-          <h5 className="w3-center w3-padding-64">
-            <span className="w3-tag w3-wide">Checkout</span>
+          <h5 className="w3-center w3-padding-32">
+            <span className={styles.header}>Checkout</span>
           </h5>
           <div className="row">
             <div className="col-25">
               <div className="container">
                 <h4>
-                  Cart
-                  <span className="price" style={{ color: "black" }}>
-                    <i className="fa fa-shopping-cart" />
-                    <b>4</b>
-                  </span>
+                  Your items:
                 </h4>
-                <p>
-                  <a href="#">Product 1</a> <span className="price">$15</span>
-                </p>
-                <p>
-                  <a href="#">Product 2</a> <span className="price">$5</span>
-                </p>
-                <p>
-                  <a href="#">Product 3</a> <span className="price">$8</span>
-                </p>
-                <p>
-                  <a href="#">Product 4</a> <span className="price">$2</span>
-                </p>
-                <hr />
-                <p>
-                  Total{" "}
-                  <span className="price" style={{ color: "black" }}>
-                    <b>$30</b>
-                  </span>
-                </p>
+                <table id={styles.itemTable}>
+                  <thead>
+                    <tr>
+                      <th>Item</th><th>Quantity</th><th>Total Price</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cart.map(c =>
+                      <tr key={c.product.id}>
+                        <td>{c.product.name}</td>
+                        <td>{c.quantity_in_cart}</td>
+                        <td>${formatPrice(c.product.price * c.quantity_in_cart)}</td>
+                      </tr>
+                    )}
+                    <tr><td>Your total:</td><td /><td>${formatPrice(totalPrice)}</td></tr>
+                  </tbody>
+                </table>
               </div>
             </div>
             <br />
             <div className="col-75">
               <div className="container">
-                <form action="/action_page.php">
-                  <div className="row">
-                    <div className="col-50">
+                <form>
+                  <div className={styles.formsRow}>
+                    <div className={styles.inputForm}>
                       <h3>Billing Address</h3>
                       <label htmlFor="fname">
                         <i className="fa fa-user" /> Full Name
@@ -80,28 +84,17 @@ const Checkout = () => {
                       </div>
                     </div>
                     <br />
-                    <div className="col-50">
+                    <div className={styles.inputForm}>
                       <h3>Payment</h3>
-                      <label htmlFor="fname">Accepted Cards</label>
-                      <div className="icon-container">
-                        <i className="fa fa-cc-visa" style={{ color: "navy" }} />
-                        <i className="fa fa-cc-amex" style={{ color: "blue" }} />
-                        <i
-                          className="fa fa-cc-mastercard"
-                          style={{ color: "red" }}
-                        />
-                        <i
-                          className="fa fa-cc-discover"
-                          style={{ color: "orange" }}
-                        />
-                      </div>
-                      <label htmlFor="cname">Name on Card:</label>
+                      <label>Accepted Cards</label>
+                      <br />
+                      <label htmlFor="cname">Name on Card</label>
                       <input type="text" id="cname" name="cardname" />
                       <br />
-                      <label htmlFor="ccnum">Credit card number:</label>
+                      <label htmlFor="ccnum">Credit card number</label>
                       <input type="text" id="ccnum" name="cardnumber" />
                       <br />
-                      <label htmlFor="expmonth">Expires:</label>
+                      <label htmlFor="expmonth">Expires</label>
                       <input type="text" id="expmonth" name="expmonth" />
                       <br />
                       <div className="row">
