319406126d53dc80fae37f30a2852a20f167087e
Mitch J <mjacovet@kent.edu>
2023-04-23 23:10:52 -0400

    add links to search results

--- components/product-search-result-item.jsx
+++ components/product-search-result-item.jsx
@@ -1,13 +1,16 @@
 import styles from "../styles/search.module.css"
+import Link from "next/link";
 import Image from 'next/image'
 
 export default function ProductSearchResultItem({ product }) {
     return (
         <div className={styles.searchResult}>
-            <Image className={styles.productImage} width={200} height={200} src={'/' + product["photo_file_name"]} alt="cupcake"/>
-            <h1 className={styles.productTitle}>{product["name"]}</h1>
+            <Link href={`/productinfo?product=${product.id}`}>
+                <Image className={styles.productImage} width={200} height={200} src={'/' + product["photo_file_name"]} alt="cupcake" />
+                <h1 className={styles.productTitle}>{product["name"]}</h1>
+            </Link>
             <p className={styles.productDesc}>{product["description"]}</p>
-            <i className={styles.productPrice}>${product["price"]/100}</i>
+            <i className={styles.productPrice}>${product["price"] / 100}</i>
         </div>
     );
 }
