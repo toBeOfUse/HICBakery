1c2d03004f76d191b1eb5e70bfbe5b38d5a265d6
Mitch J <mjacovet@kent.edu>
2023-04-24 21:32:25 -0400

    more beautiful categories with transitions

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,23 @@
 import Head from "next/head";
 import { Fragment } from "react";
+import { Reveal } from "react-awesome-reveal";
+import { keyframes } from "@emotion/react";
 import Header from "../components/Header.jsx";
 import Footer from "../components/footer.jsx";
 import styles from "../styles/Home.module.css";
 import db from "../components/db.js";
 import Link from "next/link.js";
+
+const nearbyLeftSwoosh = keyframes`
+from {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+to {
+  opacity: 1;
+  transform: translateX(0px);
+}
+`
 
 export function getServerSideProps(context) {
   // this code will run in our next.js server application, outside of the
@@ -48,26 +61,35 @@ export default function Home({ categories }) {
     <div className={styles.container}>
       <Header collapsed={false} />
       <div className={styles.categoriesContainer}>
+        <h2>Featured Product Categories:</h2>
         {categories.map((c, i) =>
-          <div key={c[0].category_name} className={styles.category}
-            style={{
-              flexDirection: i % 2 == 0 ? "row" : "row-reverse",
-              backgroundColor: i % 2 == 0 ? "lavender" : "pink"
-            }}>
-            <ProductLink product={c[0]}>
-              <img title={c[0].name} src={c[0].photo_file_name} alt={`Picture of ${c[0].name}`} />
-            </ProductLink>
-            <div className={styles.categoryTextContainer}>
-              <h3><Link href={`/search?category_filter=${c[0].category_name}`}>{c[0].category_name}</Link></h3>
-              <p>{c[0].category_description}</p>
-              {c.map(
-                (p, i) => <Fragment key={p.id} >
-                  <ProductLink product={p}>{p.name}</ProductLink>
-                  {i != c.length - 1 && <span> | </span>}
-                </Fragment>)
-              }
+          <Reveal keyframes={nearbyLeftSwoosh} key={c[0].category_name} triggerOnce>
+            <div className={styles.category}
+              style={{
+                flexDirection: i % 2 == 0 ? "row" : "row-reverse",
+                backgroundColor: i % 2 == 0 ? "lavender" : "pink"
+              }}>
+              <ProductLink product={c[0]}>
+                <img title={c[0].name} src={c[0].photo_file_name} alt={c[0].names} />
+              </ProductLink>
+              <div className={styles.categoryTextContainer}>
+                <h3>
+                  <Link href={`/search?category_filter=${c[0].category_name}`}>
+                    {c[0].category_name}
+                  </Link>
+                </h3>
+                <p>{c[0].category_description}</p>
+                <div style={{ display: "flex", flexWrap: "wrap", wordWrap: "nowrap" }}>
+                  {c.map(
+                    (p, i) => <span key={p.id} >
+                      <ProductLink product={p}>{p.name}</ProductLink>
+                      {i != c.length - 1 && <span>&nbsp;|&nbsp;</span>}
+                    </span>)
+                  }
+                </div>
+              </div>
             </div>
-          </div>
+          </Reveal>
         )}
       </div>
       <Footer />
